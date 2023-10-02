762ac0e72f1ea9876aeaa447590bed8dde479891
eric githaiga <ericgithaiga007@gmail.com>
2023-10-02 20:16:59 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form");
+
+  form.addEventListener("submit", function(event) {
+      event.preventDefault();
+      const task = event.target.Description.value;
+      const list = document.getElementById("tasks");
+      const item = document.createElement("li");
+      item.innerText = task;
+      list.appendChild(item);
+
+      console.log(task);
+  });
 });
